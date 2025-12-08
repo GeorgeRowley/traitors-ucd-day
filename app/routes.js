c24fb6c1d25b71261316e355a85977c74fc04628
George Rowley <george.rowley@nhsbsa.nhs.uk>
2025-12-08 12:34:13 +0000

    national faithful number page w/ error messaging

--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,24 @@
 // External dependencies
 const express = require('express')
-
 const router = express.Router()
 
-// Add your routes here - above the module.exports line
+// National faithful number
+router.post('/national-faithful-number', function(req, res) {
+    let number = req.session.data['nfs-number']
+
+    // Number field empty, show error
+    if (!number || number.trim() === "") {
+        // Don't proceed, render page with error message - message set in the view
+        return res.render("national-faithful-number", {
+            errorSummary: true
+        });
+    } else if (number == "123"){
+        // If number entered is 123 then proceed to this page:
+        res.redirect("correct")
+    } else {
+        // If any other number is entered, then proceed to this page:
+        res.redirect("incorrect")
+    }
+})
 
 module.exports = router
